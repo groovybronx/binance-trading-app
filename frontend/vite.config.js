b2fe091e01af1b22e85aa0fa398f453709421762
groovybronx <michelswork@yahoo.fr>
2025-02-19 23:48:52 +0100

    Remove unused React and ReactDOM server components, update production module paths to use minified versions, and add Jest configuration for testing.  DEBUT FRONTEND

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,10 @@
-
-// vite.config.js
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  build: {
-    outDir: 'dist', // <-- Par défaut
-  }
-})
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
